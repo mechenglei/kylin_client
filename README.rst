8d24f17ae211f79de905b6e97df412c9b62e8a28
程磊 <chenglei@e.hunantv.com>
2017-12-11 17:13:11 +0800

    init

---
+++ README.rst
@@ -0,0 +1,26 @@
+=======
+Summary
+=======
+
+kylin_client is a api client for kylin.
+It currently supports the versions from **2.6
+to 3.6** (users of Python 2.4 and 2.5 may use
+`2.1.3 <https://pypi.python.org/pypi?name=psutil&version=2.1.3&:action=files>`__ version).
+`PyPy <http://pypy.org/>`__ is also known to work.
+
+
+==============
+Example usages
+==============
+
+QUERY
+===
+
+.. code-block:: python
+
+    >>>from kylin_client import KylinClient as kyclient
+    >>>kyclient()
+    >>>kyclient(host='127.0.0.1:7070', user='ADMIN', password='KYLIN')
+    >>>kyclient.query('select * from kylin_test', project='kylin_test', limit=500)
+
+
---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'chenglei'
+
+from setuptools import setup, find_packages
+
+setup(
+    name='kylin_client',
+    version='0.0.1',
+    description=(
+        'kylin api client'
+    ),
+    long_description=open('README.rst').read(),
+    author='chenglei',
+    author_email='mechenglei@163.com',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=['win7', 'linux'],
+    url='https://github.com/mechenglei/kylin_client',
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
